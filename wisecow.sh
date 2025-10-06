21ac38432a837db0fd2b94c0511500a836e77d96
pavankumar3108 <168734325+pavankumar3108@users.noreply.github.com>
2025-10-07 01:44:18 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -25,12 +25,12 @@ EOF
 }
 
 prerequisites() {
-  for cmd in cowsay fortune nc; do
-    if ! command -v "$cmd" >/dev/null 2>&1; then
-      echo "Missing prerequisite: $cmd"
-      exit 1
-    fi
-  done
+    command -v cowsay >/dev/null 2>&1 && \
+    command -v fortune >/dev/null 2>&1 && \
+    command -v nc >/dev/null 2>&1 || {
+        echo "Install prerequisites."
+        exit 1
+    }
 }
 
 
